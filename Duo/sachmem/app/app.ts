b9071819b01e11d35e31fff2dac9db87d1d144e7
Khoa V. Nguyen <khoa.uet58@gmail.com>
2016-09-19 13:28:37 +0700

    init

---
+++ Duo/sachmem/app/app.ts
@@ -0,0 +1,44 @@
+import { Component, ViewChild } from '@angular/core';
+import { ionicBootstrap, Platform, Nav } from 'ionic-angular';
+import { StatusBar } from 'ionic-native';
+
+import { Page1 } from './pages/page1/page1';
+import { Page2 } from './pages/page2/page2';
+
+@Component({
+  templateUrl: 'build/app.html'
+})
+class MyApp {
+  @ViewChild(Nav) nav: Nav;
+
+  rootPage: any = Page1;
+
+  pages: Array<{title: string, component: any}>;
+
+  constructor(public platform: Platform) {
+    this.initializeApp();
+
+    // used for an example of ngFor and navigation
+    this.pages = [
+      { title: 'Page uno', component: Page1 },
+      { title: 'Page dos', component: Page2 }
+    ];
+
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      StatusBar.styleDefault();
+    });
+  }
+
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
+  }
+}
+
+ionicBootstrap(MyApp);
