1615e4c6085645c227055b3c864ae556635e20d2
Duc Anh <ducanh54@gmail.com>
2016-09-24 23:33:27 +0700

    set event click course

---
+++ PATH/sep13-enguliz/app/detail/detail.component.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by Thinking on 09/24/2016.
+ */
+import {Component} from "@angular/core";
+@Component({
+    templateUrl: 'app/detail/detail.component.html'
+})
+
+export class DetailComponent {
+    
+}
