d17bf4761aba150780b9ac49940e9222773e826c
trieudh <trieudh@icts.vn>
2016-09-29 20:11:47 +0700

    replace hard-coded path-to-model with dynamic path

--- NaVi/abchatbot-service/src/main/java/com/chatbot/Classifier.java
+++ NaVi/abchatbot-service/src/main/java/com/chatbot/Classifier.java
@@ -8,17 +8,8 @@ import org.codehaus.jettison.json.JSONObject;
  * Created by trieudh on 28/09/2016.
  */
 public class Classifier {
-//    public static void main(String[] args) {
-//        Classifier cl = new Classifier();
-//        try {
-//            System.out.print(cl.classify("lam bai tap nghe"));
-//        }
-//        catch (FileNotFoundException e) {
-//            System.out.print(e);
-//        }
-//    }
     private String classLabel = "";
-    private DomainClassifier domainClassifier = new DomainClassifier("/home/trieudh/Desktop/int3507-2016/NaVi/abchatbot-service/data/");
+    private DomainClassifier domainClassifier = new DomainClassifier(System.getProperty("user.dir") + "/data/");
 
     String classify(String userMessage) {
         domainClassifier.init();
