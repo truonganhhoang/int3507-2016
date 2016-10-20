3922b35ad72979dee84b448bb410f28d427de781
Khoa V. Nguyen <khoa.uet58@gmail.com>
2016-10-20 11:59:01 +0700

    feature: show guides for each game

--- Duo/sachmem3/src/providers/helper-service.ts
+++ Duo/sachmem3/src/providers/helper-service.ts
@@ -1,10 +1,25 @@
 import { Injectable } from '@angular/core';
+import { ToastController } from 'ionic-angular';
 
 @Injectable()
-export class HelperService { 
-  
+export class HelperService {
+  constructor(public toastCtrl: ToastController) { }
+
   random(range): number {
     return Math.floor(Math.random() * range);
+  }
+
+  presentToast(txt) {
+    let toast = this.toastCtrl.create({
+      message: txt,
+      duration: 5000,
+      position: 'bottom',
+      showCloseButton: true,
+      // closeButtonText: 'Got it!',
+      dismissOnPageChange: true
+    });
+
+    toast.present();
   }
   
 }
