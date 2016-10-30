2d712e8f6102586e032a748372d1f927c7127e31
tracquangthinh <tracquangthinh@gmail.com>
2016-10-31 00:57:20 +0700

    Persistent menu

--- DTQ/fb/recieve_message.py
+++ DTQ/fb/recieve_message.py
@@ -22,84 +22,92 @@ def recieve(data):
   if data["object"] == "page":
 
     for entry in data["entry"]:
-      log(entry["messaging"])
       for messaging_event in entry["messaging"]:
-        if messaging_event.get("message"):  # someone sent us a message
 
-          sender_id = messaging_event["sender"]["id"]    # the facebook ID of the person sending you the message
-          recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-          message_text = messaging_event["message"]["text"].lower()  # the message's text
-          luis = Luis()
-          
-          if "quick_reply" in messaging_event["message"]:
-            payload = messaging_event["message"]["quick_reply"]["payload"]
-            if PostBack.PAY_LOAD_WORD in payload:
-              word_id = int(payload.split(" ")[1])
-              user = UserRecord.get(sender_id)
-              for word_result in user.word_results:
-                if word_result.word_id == word_id:
-                  word_result.is_learned = True
-                  db.session.add(word_result)
-                  db.session.commit()
+        if messaging_event.get("postback"):
+          sender_id = messaging_event["sender"]["id"]
+          payload = messaging_event["postback"]["payload"]
+          if PostBack.PAY_LOAD_MENU_HELP in payload:
+            send_text_message(sender_id, "We have two functions for you: learn new words and do exercises. Please type 'learn word' to learn new words and 'do exercise' to do exercises. Thanks <3")
+        else:
+          if messaging_event.get("message"):  # someone sent us a message
 
-                  user.word_learned += 1
-                  db.session.add(user)
-                  db.session.commit()
+            sender_id = messaging_event["sender"]["id"]    # the facebook ID of the person sending you the message
+            recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+            message_text = messaging_event["message"]["text"].lower()  # the message's text
+            luis = Luis()
+
+            if "quick_reply" in messaging_event["message"]:
+              payload = messaging_event["message"]["quick_reply"]["payload"]
+ 
+              if PostBack.PAY_LOAD_WORD in payload:
+                word_id = int(payload.split(" ")[1])
+                user = UserRecord.get(sender_id)
+                for word_result in user.word_results:
+                  if word_result.word_id == word_id:
+                    word_result.is_learned = True
+                    db.session.add(word_result)
+                    db.session.commit()
+
+                    user.word_learned += 1
+                    db.session.add(user)
+                    db.session.commit()
                 
-                  if (user.word_learned - 1) % 3 and user.word_learned > 0:
-                    UserRecord.set_state(sender_id, UserRecord.UserState.TESTING_WORD)
-                    send_random_type_question(sender_id)
-                  else:
-                    send_new_word(sender_id)
-            else:
-              if PostBack.PAY_LOAD_QUESTION in payload:
-                if "True" in payload:
-                  send_text_message(sender_id, "Correct")
-                else:
-                  answer_right = payload.split("_")[2].upper()
-                  send_text_message(sender_id, "Wrong. Correct answer is " + answer_right)
-                time.sleep(1)
-                send_word_not_learn(sender_id)
-                UserRecord.set_state(sender_id, UserRecord.UserState.NONE)
+                    if (user.word_learned - 1) % 3 and user.word_learned > 0:
+                      UserRecord.set_state(sender_id, UserRecord.UserState.TESTING_WORD)
+                      send_random_type_question(sender_id)
+                    else:
+                      send_new_word(sender_id)
               else:
-                if PostBack.PAY_LOAD_DO_EXERCISE in payload:
+                if PostBack.PAY_LOAD_QUESTION in payload:
                   if "True" in payload:
                     send_text_message(sender_id, "Correct")
-                    log("Gui Correc")
                   else:
                     answer_right = payload.split("_")[2].upper()
                     send_text_message(sender_id, "Wrong. Correct answer is " + answer_right)
                   time.sleep(1)
-                  send_random_type_question(sender_id, do_exercise=True)
-
-          else:
-            if message_text:
-
-              cvs = db.session.query(Conversation)\
-                .filter(Conversation.question == message_text).first()
-              # log(cvs)
-              if cvs is None:
-                answer = luis.get_answer(message_text)
-                indent = luis.get_type_activity(answer)
-                entity = luis.get_entity(answer)
-                cvs = Conversation(message_text, sender_id, indent,
-                  entity)
-                db.session.add(cvs)
-                db.session.commit()
-              log(cvs)
-              
-              if ConversationRecord.is_learn_new_word(cvs):
-                if UserRecord.get_state(sender_id) == UserRecord.UserState.NONE:
                   send_word_not_learn(sender_id)
+                  UserRecord.set_state(sender_id, UserRecord.UserState.NONE)
                 else:
-                  if UserRecord.get_state(sender_id) == UserRecord.UserState.TESTING_WORD:
-                    send_random_type_question(sender_id)
-              else:
-                if ConversationRecord.is_do_exercises(message_text):
-                  send_random_type_question(sender_id, do_exercise=True)
+                  if PostBack.PAY_LOAD_DO_EXERCISE in payload:
+                    if "True" in payload:
+                      send_text_message(sender_id, "Correct")
+                      log("Gui Correc")
+                    else:
+                      answer_right = payload.split("_")[2].upper()
+                      send_text_message(sender_id, "Wrong. Correct answer is " + answer_right)
+                    time.sleep(1)
+                    send_random_type_question(sender_id, do_exercise=True)
+
+            else:
+              if message_text:
+
+                cvs = db.session.query(Conversation)\
+                  .filter(Conversation.question == message_text).first()
+                # log(cvs)
+                if cvs is None:
+                  answer = luis.get_answer(message_text)
+                  indent = luis.get_type_activity(answer)
+                  entity = luis.get_entity(answer)
+                  cvs = Conversation(message_text, sender_id, indent,
+                    entity)
+                  db.session.add(cvs)
+                  db.session.commit()
+              # log(cvs)
+              
+                if ConversationRecord.is_learn_new_word(cvs):
+                  if UserRecord.get_state(sender_id) == UserRecord.UserState.NONE:
+                    send_word_not_learn(sender_id)
+                  else:
+                    if UserRecord.get_state(sender_id) == UserRecord.UserState.TESTING_WORD:
+                      send_random_type_question(sender_id)
                 else:
-                  botsay = ProgramO.get_answer(sender_id, message_text)
-                  send_text_message(sender_id, botsay)
+                  if ConversationRecord.is_do_exercises(message_text):
+                    log("LAM BAI TAP")
+                    send_random_type_question(sender_id, do_exercise=True)
+                  else:
+                    botsay = ProgramO.get_answer(sender_id, message_text)
+                    send_text_message(sender_id, botsay)
 
 def send_question(sender_id, do_exercise=False):
   random_result = WordResultRecord.get_random_result(sender_id)
