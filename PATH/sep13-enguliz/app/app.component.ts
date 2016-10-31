ceb505c359cba21002e486eb9f2187751f07f924
Duc Anh <ducanh54@gmail.com>
2016-10-31 10:31:51 +0700

    Merge branch 'master' of https://github.com/ducanhk58uet/int3507-2016
    # Conflicts:
    #	PATH/sep13-enguliz/index.html

--- PATH/sep13-enguliz/app/app.component.ts
+++ PATH/sep13-enguliz/app/app.component.ts
@@ -8,8 +8,93 @@ import {User} from "./user/user.model";
 @Component({
     selector: 'my-app',
     template: `
-          
-        <router-outlet></router-outlet>`,
+        <section id="head">
+		<div id="mySidenav" class="sidenav">
+		  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+		  <div class="row fix-menu">
+		  	<div class="fix-col col-xs-12">
+				<ul class="multi-column-dropdown">
+		            <li><a href="#">Listening</a></li>
+		            <li class="divider"></li>
+		            <li><a href="#">Reading</a></li>
+		            <li class="divider"></li>
+		            <li><a href="#">Grammar</a></li>
+	            </ul>
+			</div>
+		  </div>
+		  	  
+		</div>
+		<div id="mySidenav" style="background-color: #27A8E6;" class="mobile-only">		
+			<nav class="navbar navbar-inverse" role="navigation">		
+			    <div class="navbar-header">
+			    <div class="fix-icon"><span onclick="openNav()">&#9776;</span></div>
+			    <a class="navbar-brand viademy-logo" [routerLink]="['']">
+	                Enguliz
+	            </a>			   
+			    </div>
+			    
+			</nav>
+		</div>
+		<nav class="navbar navbar-inverse hidden-xs">
+			<div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<a class="navbar-brand viademy-logo" [routerLink]="['']">
+						Enguliz
+					</a>
+				</div>
+	<!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="main-menu-categories">
+      <ul class="nav navbar-nav">
+        <li class="dropdown active">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+            <i class="fa fa-bars" aria-hidden="true"></i>
+            Danh mục 
+            <span class="caret"></span></a>
+          	<ul class="dropdown-menu multi-column columns-1">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <ul class="multi-column-dropdown">
+                            <li><a href="#">Listening</a></li>
+                            <li class="divider"></li>
+                            <li><a href="#">Reading</a></li>
+                            <li class="divider"></li>
+                            <li><a href="#">Grammar</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </ul>
+    	</li>
+	</ul>
+	<form class="navbar-form navbar-left">
+	    <div class="form-group" id="main-menu-course-search-box">
+	        <i id="main-menu-search-icon" class="fa fa-search" aria-hidden="true"></i>
+	      <input type="text" id="main-menu-course-search" class="form-control" placeholder="Tìm đề tài">
+	    </div>    
+	</form>
+	<ul class="nav navbar-nav navbar-right hidden-sm">
+	    <li><a [routerLink]="['register']"><div id="main-menu-register">Đăng ký</div></a></li>
+	    <li><a [routerLink]="['login']">Đăng nhập</a></li>
+	</ul>
+	</div><!-- /.navbar-collapse -->
+</div><!-- /.container-fluid -->
+</nav>
+	</section>      
+        <router-outlet></router-outlet>
+        <script type="text/javascript" src="js/jquery-1.6.min.js"></script>
+<script type="text/javascript" src="js/skinable_tabs.min.js"></script>
+<script type="text/javascript">
+    $('.tabs_holder').skinableTabs({
+        effect: 'basic_display',
+        skin: 'skin10',
+        position: 'top'
+    });
+</script>
+<!-------------------- JS -------------------->
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script type="text/javascript" src="js/bootstrap-3.1.1.min.js"></script>
+<script type="text/javascript" src="js/custom.js"></script>
+<script type="text/javascript" src="js/menu.js"></script>`,
     providers: [UserService]
 })
 
