45cf4f3bb681684011b836bb12613c0dd4e2926f
CuLac <ngvietdung95@gmail.com>
2016-11-03 22:09:22 +0700

    1

---
+++ NSFW/FAQ Bot V2/Sql Script/SQLQuery5.sql
@@ -0,0 +1,28 @@
+﻿insert into dbo.help(category_id,question,sub_question,answer,link) values
+(5,'Groove Music help','What is the Microsoft Groove Music app?',
+'Microsoft Groove Music is brand new for Windows 10. It plays your music collection and lets you make and listen to playlists. Add your MP3s to OneDrive and you can use the Groove Music app to play your songs on other devices, too—PCs, tablets, Windows Phone, and Xbox—for free.
+The app is best, however, when you use it with a Groove Music Pass. For a low monthly fee, Groove Music Pass lets you stream and download music from one of the biggest music catalogs on the planet. Custom radio stations play hours of music based on your favorite artists. Use Groove Music Pass on your PC, tablet, phone (including iOS and Android), and Xbox.',
+'https://support.microsoft.com/en-us/instantanswers/f63cd30e-1a6c-404f-8aa7-4538ddafe775/what-is-the-microsoft-groove-music-app'),
+(5,'Groove Music help','Play music in the Groove app on Windows 10',
+'In Windows 10, Groove Music is pinned on Start. You''ll also see it listed alphabetically in All apps.
+Compatible song files in your Music folder will appear in the app. To point Groove to additional folders, launch the app and go to  Settings > Preferences > Choose where we look for music on this PC.',
+'https://support.microsoft.com/en-us/instantanswers/fb1b19a8-56ee-45b4-b4a2-afa39808d1e3/play-music-in-the-groove-app-on-windows-10'),
+(5,'Groove Music help','Play music with Groove',
+'Enjoy all the music you love in one simple, fast, and elegant Windows 10 app.
+By itself, Groove makes it easy to play and manage your music and playlists. But when you add your songs to , Groove lets you play them—for free—on all your favorite devices: PC, Xbox, Android, iPhone, Windows phone, Sonos, and the web.
+On your PC, just add your MP3s, even iTunes songs, to your music folder on OneDrive. Then you’ll have them at your fingertips in Groove on any of your devices.
+Want more ways to play? Get a [Groove music Pass](https://www.microsoft.com/en-us/store/entertainment) and dive into artist-based radio, curated playlists, and one of the biggest music catalogs on the planet. Groove Music Pass lets you stream and download new hits and familiar favorites from a selection of over 40 million songs. Listen—ad-free—just about anywhere.
+
+`Note`
+>Groove Music Pass is sold separately and it''s not available in all markets. The catalog also varies by market.',
+'https://support.microsoft.com/en-us/help/17186'),
+(5,'Photos help','Add pictures to the Photos app',
+'The Photos app automatically finds most of the photos and videos on your PC and OneDrive. But if it’s missing a folder full of photos, you might need to tell it where to look.
+* Open the Photos app and select the Settings icon.
+* Under Sources, select Add a folder.
+* Browse to a folder on your PC, an external drive, or a network drive connected to your PC, and then select Add this folder to Pictures. Subfolders of the folders you add are included too.',
+'https://support.microsoft.com/en-us/instantanswers/7870f4ac-0cfe-4f29-b19b-fbf064144cbc/add-pictures-to-the-photos-app'),
+(5,'Photos help','Import photos from phone to PC',
+'* To transfer photos from phone to PC, connect your phone to the PC with a USB cable. Then on the PC, open the Photos app (select the  Start button, and then  Photos). Select Import, and follow the instructions. It''ll automatically select new items you haven''t imported before, or you can choose what to import.
+* Or, skip the wires and [use the Windows Phone Companion app](http://go.microsoft.com/fwlink/?LinkId=619276) on your PC to automatically upload photos from your phone as you take them.',
+'https://support.microsoft.com/en-us/instantanswers/942ddea8-e901-125c-b08e-a9e10315538c/import-photos-from-phone-to-pc');
