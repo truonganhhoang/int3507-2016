c2dfebd5eb4c193460b5b484e1de7c98f88b3f79
vantayden <vantayden@gmail.com>
2016-11-16 19:07:44 +0700

    Update Get User profile in core.php

--- ACE/facebookChatbot/core.php
+++ ACE/facebookChatbot/core.php
@@ -247,4 +247,43 @@ class FbBotApp{
     }
 }
 
+class UserProfile
+{
+    protected $data = [];
+
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    public function getFirstName()
+    {
+        return $this->data['first_name'];
+    }
+
+    public function getLastName()
+    {
+        return $this->data['last_name'];
+    }
+
+    public function getPicture()
+    {
+        return $this->data['profile_pic'];
+    }
+
+    public function getLocale()
+    {
+        return $this->data['locale'];
+    }
+
+    public function getTimezone()
+    {
+        return $this->data['timezone'];
+    }
+
+    public function getGender()
+    {
+        return $this->data['gender'];
+    }
+}
 ?>
